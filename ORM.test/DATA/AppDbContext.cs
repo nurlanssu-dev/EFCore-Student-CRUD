3c6d1049ee3fea24d4f38621a0693e91776b56b3
nurlanssu <nurlanssu@code.edu.az>
2026-09-10 17:44:43 +0400

    add home task

---
+++ ORM.test/DATA/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ORM.test.Models;
+
+namespace ORM.test.DATA;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server=localhost;Database=StudentOrmDb;Trusted_Connection=True;TrustServerCertificate=True;"
+        );
+    }
+}
---
+++ ORM.test/Program.cs
@@ -0,0 +1,57 @@
+﻿using ORM.test.DATA;
+using ORM.test.Models;
+
+
+//-------------------Task2-----------------
+
+
+//AppDbContext context = new AppDbContext();
+
+//Console.Write("Enter name: ");
+//string name = Console.ReadLine()!;
+
+//Console.Write("Enter age: ");
+//int age = int.Parse(Console.ReadLine()!);
+
+//Student student = new Student
+//{
+//    Name = name,
+//    Age = age
+//};
+
+//context.Students.Add(student);
+
+//context.SaveChanges();
+
+//Console.WriteLine("Student added successfully.");
+
+//-------------------Task3-----------------
+
+//AppDbContext context = new AppDbContext();
+
+//var students = context.Students
+//    .ToList();
+
+//foreach (var student in students)
+//{
+//    Console.WriteLine($"ID : {student.Id} StudentName: {student.Name} Age: {student.Age}");
+//}
+
+//-------------------Task4-----------------
+
+AppDbContext context = new AppDbContext();
+
+Console.Write("Enter student name: ");
+string name = Console.ReadLine()!;
+
+var student = context.Students
+    .FirstOrDefault(s => s.Name == name);
+
+if (student != null)
+{
+    Console.WriteLine($"ID: {student.Id} StudentName: {student.Name} Age: {student.Age}");
+}
+else
+{
+    Console.WriteLine("Student not found.");
+}
